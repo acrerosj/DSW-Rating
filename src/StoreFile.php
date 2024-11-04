dfffa656bfc6615db235067b4adc12981ec65e66
Andrés Crespo <acrerosj@canariaseducacion.es>
2024-11-04 17:15:48 +0000

    save file method

--- src/StoreFile.php
+++ src/StoreFile.php
@@ -1,9 +1,16 @@
 <?php
 namespace Dsw\Rating;
 
-class StoreFile implements StoreInterface {
+use DateTime;
 
-  public function addRate(int $rate) {}
+class StoreFile implements StoreInterface {
+  
+  public function addRate(int $rate) {
+    $date = new DateTime();
+    $path = '../data/';
+    $fileName = $path . $date->format('Y_m_d_H_i') . '.csv';
+    file_put_contents($fileName, $rate . ',' , FILE_APPEND);
+  }
 
   public function getStadistics(): array 
   {
